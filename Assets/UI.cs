c677b90f2525a700ae370803ee7433f8818cb46c
Arthur Lee <lpsa7@ABCs-MacBook-Pro.local>
2023-07-04 14:18:10 +0800

    added function to shuffle deck

--- Assets/UI.cs
+++ Assets/UI.cs
@@ -15,12 +15,62 @@ public class UI : MonoBehaviour
     public Button button;
     public GameObject card;
     public GameObject trash;
+    public GameObject Deck;
+    Random rnd;
     
     void Start()
     {
-        
+        for(int i = 0; i < 52; i++){
+            initDeck(i+1);
+        }
+        rnd = new Random((int)Time.time*1000);
     }
-
+    public void initDeck(int count){
+        
+        GameObject prefab = card;
+        GameObject player = Instantiate(prefab) as GameObject;
+        player.transform.position = new Vector3(0,0,2);
+        player.transform.localScale = new Vector3(1, 1, 1);
+        player.transform.SetParent(Deck.transform);
+        player.transform.position = Deck.transform.position;
+        player.name = "Card" + (count);
+        player.GetComponentInChildren<Text>().text = count+"";
+    }
+    public void clearDeck(){
+        foreach(GameObject c in GameObject.FindGameObjectsWithTag("Card")){
+            if(!c.gameObject.transform.IsChildOf(trash.transform)){
+                c.transform.SetParent(trash.transform);
+                c.transform.position = trash.transform.position;
+                c.GetComponent<CardEvent>().unhighlight();
+                c.GetComponent<CardEvent>().shrinken();
+            }
+        }
+    }
+    public void shuffleDeck(){
+        int i = 0;
+        while(trash.transform.childCount>0){
+            i = rnd.Next(0,trash.transform.childCount);
+            GameObject card = trash.transform.GetChild(i).gameObject;
+            card.transform.SetParent(Deck.transform);
+            card.transform.position = Deck.transform.position;
+        }
+    }
+    public void drawCard(){
+        if(Deck.transform.childCount>0){
+            GameObject card = Deck.transform.GetChild(0).gameObject;
+            GameObject goParent = GameObject.Find("Cards");
+            int count = 0;
+            for(int i = 0; i < goParent.transform.childCount; i++){
+                if(goParent.transform.GetChild(i).tag == "Card"){
+                    count++;
+                }
+            }
+            if(count >= 7){
+                return;
+            }
+            card.transform.SetParent(goParent.transform);
+        }
+    }
     public void createCard(){
         GameObject goParent = GameObject.Find("Cards");
         int count = 0;
